9292764e3bf1f5fc6169ba8619b76ea9e61e770d
Israel Tavares <israelbulcaotavares@gmail.com>
2023-10-29 17:41:15 -0300

    revisão na sintaxe

---
+++ 11_expressao_de_funcao/expressoes_funcao.js
@@ -0,0 +1,19 @@
+// function minhaFuncao(params){
+
+// }
+
+// minhaFuncao("Param")
+
+//expressão de função
+
+// console.log(soma(1,2))
+
+console.log(apresentar())
+
+function apresentar(){
+    return "Olá"
+}
+
+//console.log(soma(2,4)) // não funciona antes da exmpressão function abaixo, tem que depois da function
+const soma = function(num1, num2) {return num1 + num2}
+console.log(soma(2,4))
